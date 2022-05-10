87535fe63fbaac7150b4d08f922fdd42d0cc237a
Facorazza <20555025+facorazza@users.noreply.github.com>
2022-05-10 14:24:41 +0200

    Move parse_custom_fields to utils.py

---
+++ utils.py
@@ -0,0 +1,23 @@
+import re
+
+
+def parse_custom_fields(fields):
+    custom_fields = list()
+    pattern = re.compile(r"(\S+), (\w+): (.+)")
+    
+    for field in pattern.findall(fields):
+        match field[1]:
+            case "text" | "email" | "file" | "website":
+                field_type = 0 # Text field
+            case "secret":
+                field_type = 1 # Hidden field
+            case _:
+                field_type = 0 # Default to text field
+    
+        custom_fields.append({
+            "name": field[0],
+            "value": field[2],
+            "type": field_type,
+        })
+    
+    return custom_fields
